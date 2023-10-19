3aecddb7cc3b5442205d652c45b3a37f5a130a76
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-19 11:17:44 -0600

    updt

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -19,7 +19,7 @@ final appRouter = GoRouter(
           path: 'movie/:id',
           name: MovieScreen.name,
           builder: (context, state) {
-            final movieId = state.params['id'] ?? 'no-id';
+            final movieId = state.pathParameters['id'] ?? 'no-id';
 
             return MovieScreen( movieId: movieId );
           },
--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/providers/storage/favorite_movies_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
